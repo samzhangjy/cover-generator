d32c77540b998c7a644be530c6d0b3cd64defa66
Sam Zhang <samzhangjy@outlook.com>
2022-06-15 19:53:08 +0800

    feat: added a 404 page

--- src/routes/notfound/index.tsx
+++ src/routes/notfound/index.tsx
@@ -1,17 +1,23 @@
-import { FunctionalComponent, h } from 'preact';
-import { Link } from 'preact-router/match';
-import style from './style.css';
+import { FunctionalComponent, h } from "preact";
+import { Link } from "preact-router/match";
+import Blobs from "../../components/Blobs";
+import style from "./style.scss";
 
 const Notfound: FunctionalComponent = () => {
-    return (
-        <div class={style.notfound}>
-            <h1>Error 404</h1>
-            <p>That page doesn&apos;t exist.</p>
-            <Link href="/">
-                <h4>Back to Home</h4>
-            </Link>
+  return (
+    <div class={style.container}>
+      <Blobs />
+      <div class={style.notfound}>
+        <div>
+          <h1>Error 404</h1>
+          <p>That page doesn't exist.</p>
+          <Link href="/">
+            Back to Home
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Notfound;
